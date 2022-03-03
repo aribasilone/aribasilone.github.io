4b1a0ed6e19b271ccf43277e7adac611dc742413
Arianna Basilone <abasilon@uwo.ca>
2022-03-03 16:35:27 -0500

    New 2022 website layout. Additional pages for freelance and skillsets.

---
+++ buttons.js
@@ -0,0 +1,16 @@
+function contactButton(){
+    var link = "mailto:abasilone.hba2020@ivey.ca"
+            + "?subject=Arianna Basilone Freelance - Request for Quote"
+            + "&body=Please send a high-level summary of the task/project and your contact information!";
+    
+    window.location.href = link;
+    // alert("If your default mail client does not automatically open, please email me at abasilone.hba2020@ivey.ca")            
+}
+
+function collabButton(){
+    var link = "mailto:abasilone.hba2020@ivey.ca"
+            + "?subject=Arianna Basilone - Collaboration Opportunity"
+            + "&body=Please send a high-level summary of the project, idea, or start-up you'd believe I'd be a good fit for. As well, briefly describe the capacity in which I would be assiting, and your contact information!";
+    
+    window.location.href = link;      
+}
---
+++ other.js
@@ -0,0 +1,158 @@
+var slideIndex, slides, dots, slideA, slideB, slideC;
+
+window.onload = function(){
+    slideIndex = 1;
+    slides = document.getElementsByClassName("slide-img-3x");
+    
+    for (let x = 0; x < slides.length; x++) {
+        var dotCont = document.getElementById("dot-container");
+        var spanElm = document.createElement("span");
+        spanElm.className="dot";
+        spanElm.addEventListener("click", selectSlide);
+        spanElm.myParam = x;
+        dotCont.appendChild(spanElm);
+    }
+    dots = document.getElementsByClassName("dot");
+    dots[slideIndex].className += " active";
+}
+    
+function arrowFunct(i){
+    //determine moving left or right
+    //default right
+    let rl=0;
+    if(i<slideIndex){
+        rl=1;
+        //moving left
+    }
+
+    //circular slideshow overindex/underindex
+    if (i >= slides.length) {i = 0}
+    if (i < 0) {i = slides.length-1}
+
+    //for now hard code since only 3 images
+    //for future use, implement a structure like a double linkedlist
+        
+    //ensure array index is accessible, i.e. if left index not accessible loop to last index 
+    if(slideIndex-1 < 0){
+        slideA=slides[slides.length-1].src;
+    }
+    else{
+        slideA = slides[slideIndex-1].src; 
+    }
+    
+    //slide currenlty selected, i.e. where the dot is
+    slideB = slides[slideIndex].src;
+
+    //ensure array index is accessible, i.e. if right index not accessible loopback to first index 
+    if(slideIndex+1 >=slides.length){
+        slideC=slides[0].src
+    }
+    else{
+        slideC = slides[slideIndex+1].src; 
+    }
+    
+    
+    //change the images
+    //move the center slide in the intended direction (left/right)
+    slides[i].src=slideB;
+    
+    //move the left slide in the intended direction, but ensure array index is accessible
+    if(i-1 < 0){
+        
+        slides[slides.length-1].src=slideA;
+        
+    }
+    else{
+        slides[i-1].src=slideA; 
+    }
+
+    //move the right slide in the intended direction, but ensure array index is accessible
+    if(i+1 >=slides.length){
+        slides[0].src=slideC;
+    }
+    else{
+        slides[i+1].src=slideC;  
+    }
+     
+    //set slide index and dots 
+    console.log(rl);
+    console.log(i);
+    console.log(slideIndex);
+    if(rl==0){
+        if(slideIndex-1<0){
+            slideIndex=slides.length-1;
+        }else{
+            slideIndex -=1;
+        }
+        
+    }
+    else if(rl==1){
+        if(slideIndex+1>=slides.length){
+            slideIndex=0;
+        }
+        else{
+            slideIndex+=1;
+        }
+    }
+    console.log(slideIndex);
+    setDots();
+
+}
+
+function slideFunct(i){
+    //check that same one not reselected
+    if(i != slideIndex){
+
+        //for now hard code since only 3 images
+        //for future use, implement a structure like a double linkedlist
+
+        switch (i){
+            case 0: 
+                slideA="resources/profilepics/concert_photography_3.JPG";
+                slideB="resources/profilepics/concert_photography_2.JPG";
+                slideC="resources/profilepics/concert_photography.JPG";
+                break;
+            case 1: ;
+                slideA="resources/profilepics/concert_photography_2.JPG";
+                slideB="resources/profilepics/concert_photography.JPG";
+                slideC="resources/profilepics/concert_photography_3.JPG";
+                break;
+            case 2: ;
+                slideA="resources/profilepics/concert_photography.JPG";
+                slideB="resources/profilepics/concert_photography_3.JPG";
+                slideC="resources/profilepics/concert_photography_2.JPG";
+                break;
+            default:  
+                slideA="resources/profilepics/concert_photography_2.JPG";
+                slideB="resources/profilepics/concert_photography.JPG";
+                slideC="resources/profilepics/concert_photography_3.JPG";
+                break;
+        }
+
+        slides[0].src=slideA;
+        slides[1].src=slideB;
+        slides[2].src=slideC;
+        
+        //set slide index
+        slideIndex=i;
+        setDots();       
+            
+    }
+
+}
+    
+function setDots(){
+    //show positioning via dots
+    Array.prototype.forEach.call(dots, elm => {
+        if (elm.myParam == slideIndex){
+            elm.className += " active";
+        }
+        else{
+            elm.className=elm.className.replace(" active", "");
+        }           
+    });    
+}
+function plusSlides(n) {arrowFunct(slideIndex + n);}
+
+function selectSlide(e) {slideFunct(e.currentTarget.myParam);}
+
